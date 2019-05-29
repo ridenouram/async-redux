93f43cadd8313dd469f8c60b6cdf2914a9d3616c
Anna Ridenour <annamridenour@gmail.com>
2019-05-29 14:20:34 -0700

    wrote logger middleware, working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import {
+  createStore,
+  applyMiddleware,
+  compose
+} from 'redux';
 
-render(
-  <App />,
-  document.getElementById('root')
+function reducer(state = {}, action) {
+  switch(action.type) {
+    case 'TESTING':
+      return 'testing';
+    default: 
+      return state;
+  }
+}
+
+const logger = store => next => action => {
+  console.log('Before reducer', store.getState());
+  next(action);
+  console.log('After Reducer', store.getState());
+};
+
+const store = createStore(
+  reducer,
+  compose (
+    applyMiddleware(
+      logger
+    ),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
+
+
+store.dispatch({
+  type: 'TESTING'
+});
+
+
